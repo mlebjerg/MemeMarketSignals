499307f2e909e841289ba45a4322408e38a68534
Mikkel <mlebjerg@me.com>
2023-07-01 01:17:21 +0200

    30jun23

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -11,8 +11,8 @@ endDate = None
 symbol = "GME"
 today = date.today()
 
-latePost = False
-specificDay = date.fromisoformat('2023-06-27')
+latePost = True
+specificDay = date.fromisoformat('2023-06-30')
 
 if latePost:
     today = specificDay
